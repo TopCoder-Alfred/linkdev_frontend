9543e35d7be839890ea50a207420f9330f77a94a
Alfred.xin <haomin.working@gmail.com>
2025-02-22 22:24:29 +1000

    change api url

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,5 +1,5 @@
 export const base = {
-  baseURL: "https://linkdev-backend.onrender.com/",
+  baseURL: "https://linkdev-backend.onrender.com",
   register: "/api/users/register",
   sendMail: "/api/email/send",
   verifyCode: "/api/email/verify",
