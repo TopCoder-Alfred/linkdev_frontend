24586594bf27ab443f6f9dfe8ba103ba7376558a
Alfred.xin <haomin.working@gmail.com>
2025-02-22 22:27:52 +1000

    change api url

--- src/components/utils/request/index.jsx
+++ src/components/utils/request/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.DEV
   ? "http://localhost:8000"
-  : "https://backend.linkdevapp.com";
+  : "https://linkdev-backend.onrender.com";
 export default axios.create({
   baseURL: `${API_URL}`,
 });
